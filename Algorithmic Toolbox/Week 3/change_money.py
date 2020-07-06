a20b7a429db3623edf434190179c7baa1ac4c66d
Shubham Giri <53148932+iamshubhamg@users.noreply.github.com>
2020-07-06 17:31:17 +0530

    Added code for Money Change

---
+++ Algorithmic Toolbox/Week 3/change_money.py
@@ -0,0 +1,10 @@
+n = int(input())
+count = 0
+for i in [10, 5, 1]:
+    if n>=i:
+        q = n//i
+        count += q
+        n = n%i
+        if n==0:
+            print(count)
+            quit()
