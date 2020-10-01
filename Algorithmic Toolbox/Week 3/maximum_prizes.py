871ad24eaf480fdec89e4c34e06e2aa4520a9cd4
lavanya2316 <51858922+lavanya2316@users.noreply.github.com>
2020-10-01 12:34:59 +0530

    Update maximum_prizes.py

--- Algorithmic Toolbox/Week 3/maximum_prizes.py
+++ Algorithmic Toolbox/Week 3/maximum_prizes.py
@@ -1,18 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool comparator(string a,string b)
+{
+    string ab,ba;
+    ab=a.append(b);
+    ba=b.append(a);
 
-n = int(input())
-if n == 1:
-    print(1)
-    print(1)
-    quit()
-W = n
-prizes = []
-for i in range(1, n):
-    if W>2*i:
-        prizes.append(i)
-        W -= i
-    else:
-        prizes.append(W)
-        break
-
-print(len(prizes))
-print(' '.join([str(i) for i in prizes]))
+    if(ab.compare(ba)>0)
+        return true;
+    return false;
+}
+void display(vector<string>v,int n)
+{
+    sort(v.begin(),v.end(),comparator);
+    for(int i=0;i<n;i++)
+    {
+        cout<<v[i];
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<string>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    display(v,n);
+    return 0;
+}
